90ce920320dd6880d1f25f2e32896ce5bf43da0e
unknown <jaylesh27@gmail.com>
2017-06-07 18:41:40 -0400

    initial setup

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '37Jl8uyHjFapWYaHHCQoqujtC',
+  consumer_secret: '02mVGoHi55eoXzprRsQRvRuNe7Xo4bP8TaktR7Va68tEDmfMEK',
+  access_token_key: '2888101095-UL8VC7doYaGuj3bg8UsRlcUGTe6Z5UdbDtEgR2g',
+  access_token_secret: 'AvVlVJMs3nd5FwLDKy95daoJ26kzU6bt0aU1H51YIVWHd',
+}
